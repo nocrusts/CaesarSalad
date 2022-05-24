8ea0b427518bd01ecabb3910d8af15b3a34eac08
Thelolguy1 <18731514+Thelolguy1@users.noreply.github.com>
2022-05-24 13:14:21 -0700

    Partial implementation of multidigit

--- src/CaesarSalad.java
+++ src/CaesarSalad.java
@@ -1,12 +1,10 @@
 //@author Curtis Lou with Anselme Sorin
-//
+//Caesar Cipher encryption / decryption method.
 //@version May 24, 2022
 
 public class CaesarSalad {
 
-	public static String caesarShift(String plaintext, int n) { // This method DOES NOT PRESERVE CASE
-		// a - z = 97 - 122
-		// A - Z = 
+	public static String caesarShift(String plaintext, int n) { // Input: Plaintext, shift integer
 		String encString = "";
 		for (int i = 0; i < plaintext.length(); i ++) {
 			int currentCharAscii = plaintext.charAt(i);
@@ -30,17 +28,50 @@ public class CaesarSalad {
 		return encString;
 	}
 	
+	//Inputs: plaintext, multidigit key, encryption mode.
+	// mode true for encryption
+	// mode false for decryption
+	public static String caesarShiftMultiDigit(String plaintext, String key, boolean mode) {   
+		int keyIdx = 0;
+		String encString = "";
+		for (int i = 0; i < plaintext.length(); i++) {
+			
+			if (keyIdx > key.length() - 1) {
+				keyIdx = 0;
+			}
+			
+			int convertedSingleKey = key.charAt(keyIdx) - 48;
+			String tempString = "";
+			tempString += plaintext.charAt(i);
+			encString += caesarShift(tempString, convertedSingleKey);
+			keyIdx++;
+		}
+		return encString;
+	}
+	
 	public static void main(String args[]) {
-		String uncMessage = "1!@/blah testing ABDCE";
-		int shiftAmt = 5;
+		String uncMessage = "i am sleepying";
+		int shiftAmt = 3;
+		String key = "2428375565"; 
 		
 		String encMessage = caesarShift(uncMessage, shiftAmt);
 		String decMessage = caesarShift(encMessage, shiftAmt * -1);
 		
+		System.out.println("Part 1a/1b:");
 		System.out.println("Current Shift: " + shiftAmt);
 		System.out.println("Raw Message (not passed through decryption): " + uncMessage);
 		System.out.println("Encrypted: " + encMessage);
 		System.out.println("Decrypted: " + decMessage);
+
+		String encMessageMulti = caesarShiftMultiDigit(uncMessage, key, true);
+		String decMessageMulti = caesarShiftMultiDigit(uncMessage, key, false);
+
+		System.out.println();
+		System.out.println("Part 2:");
+		System.out.println("Current Key: " + key);
+		System.out.println("Raw Message (not passed through decryption): " + uncMessage);
+		System.out.println("Encrypted: " + encMessageMulti);
+		System.out.println("Decrypted: " + decMessageMulti);
 	}
 
 }
